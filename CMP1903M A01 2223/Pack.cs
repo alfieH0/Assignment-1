e6b0a7b2a5bbb5ea8cb9b7250325df8b3fa2e1e4
alfieH0 <Alfieholman@gmail.com>
2023-04-28 01:33:33 +0100

    added to show specifically what cards were dealt

--- CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/Pack.cs
@@ -50,6 +50,9 @@ namespace CMP1903M_A01_2223
                     {       
                     int num1 = int.Parse(pack[0].SpecialCards);                     //converts the first card to a number
                     int num2 = int.Parse(pack[2].SpecialCards);                     //converts the second card to a number
+                    Console.WriteLine(pack[0].CardName);
+                    Console.WriteLine(pack[1].CardName);
+                    Console.WriteLine(pack[2].CardName);      
                     int CorrectAns = num1 + num2;
                     bool correct = false;                                           //used to set up a post check loop
                     do
